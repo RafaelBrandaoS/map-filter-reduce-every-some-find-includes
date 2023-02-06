0e490749d9f9f8ba823b8756c8d9f824ee79b4a5
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2023-02-06 11:03:33 -0300

    manipulador every

--- js/index.js
+++ js/index.js
@@ -29,9 +29,9 @@ const celsius = farenheight.map(convercao)
 
 /* ========== filter() ========== */
 
-const array = [4, 2, 5, 27, 14, 4, 3, 5, 2, 10]
+const filterArray = [4, 2, 5, 27, 14, 4, 3, 5, 2, 10]
 
-const removeRepitidos = array.filter((elem, index, arr) => arr.indexOf(elem) === index)
+const removeRepitidos = filterArray.filter((elem, index, arr) => arr.indexOf(elem) === index)
 
 //console.log(removeRepitidos)
 
@@ -53,6 +53,32 @@ const foguetes = [
 
 const totalDeLancamentos = foguetes.reduce( ( preVal, elem ) => preVal + elem.lancamentos, 0 )
 
-console.log(totalDeLancamentos)
+//console.log(totalDeLancamentos)
 
 /* ========== reduce() ========== */
+
+/* ========== every() ========== */
+
+// verificar se todos os elementos são maiores que 10
+const everyArray = [19, 16, 30, 22, 22, 38, 17]
+
+const maiorQueDez = everyArray.every( elem => elem >= 10)
+
+//console.log(maiorQueDez)
+
+//verificar se todos são maiores de idade
+
+const usuarios = [
+   {id: 13, nome: 'Isabelle', idade: 20},
+   {id: 17, nome: 'Rafael', idade: 18},
+   {id: 22, nome: 'Alisson', idade: 19},
+   {id: 28, nome: 'Giovana', idade: 18},
+   {id: 37, nome: 'Lara', idade: 19},
+   {id: 39, nome: 'Guilherme', idade: 22}
+]
+
+const maiorDeIdade = usuarios.every(p => p.idade >= 18)
+
+console.log(maiorDeIdade)
+
+/* ========== every() ========== */
